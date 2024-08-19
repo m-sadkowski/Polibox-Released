fcfc822994c7d01e55a4505c048aa59cc7322fe5
m-sadkowski <msadkowski000@gmail.com>
2024-08-19 17:55:49 +0200

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -15,13 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
-ALLOWED_HOSTS = []
-
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
